e3e346a048c832c7e23ed8764962958b67371ed4
visan14 <visanchikate200897@gmail.com>
2024-02-17 18:37:44 +0530

    AgsMovers Internationl Removals comapnay AGS Internation Movers

---
+++ assest/js/js.js
@@ -0,0 +1,59 @@
+function myFunction() {
+  document.getElementById("myDropdown").classList.toggle("sho");
+}
+
+// Close the dropdown menu if the user clicks outside of it
+window.onclick = function(event) {
+  if (!event.target.matches('.dropbtn')) {
+    var dropdowns = document.getElementsByClassName("drop-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('sho')) {
+        openDropdown.classList.remove('sho');
+      }
+    }
+  }
+}
+
+
+
+function myFun() {
+  document.getElementById("myDrop").classList.toggle("show");
+}
+
+// Close the dropdown menu if the user clicks outside of it
+window.onclick = function(event) {
+  if (!event.target.matches('.dropbtn')) {
+    var dropdowns = document.getElementsByClassName("drop-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
+}
+
+
+// scroll button
+
+let mybutton = document.getElementsByClassName("button");
+
+
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
